0e7146ad75a553e9fc2f139e3c0aac9754c1e32c
rbogdanov <romanlbogdanov@gmail.com>
2017-03-09 19:28:14 +0200

    1

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -19,32 +19,60 @@ class Header extends Component {
         if (user.isLogged == 'yes') {
             authTemplate = <ul className="uk-navbar-nav uk-animation-fade">
                 <li className="uk-inline">
-                    <span className="uk-text-middle">{user.name}</span>
+                    <span className="uk-text-middle uk-visible@s">{user.name}</span>
                     <div className="uk-inline">
-                        <img className="uk-border-circle uk-margin-small-left" src={user.picture} width="50" height="50"/>
+                        <img className="uk-border-circle uk-margin-small-left user-picture" src={user.picture} width="50" height="50"/>
                     </div>
                 </li>
-                <div is uk-drop="mode: click; pos: bottom-right" class="uk-card uk-card-body uk-card-default uk-width-auto uk-padding-small">
-                      <button className="uk-button uk-button-text" onClick={this.logOutFromFacebookHandler.bind(this)}>Выйти</button>
-                </div>
+                <ul is uk-drop="mode: click; pos: bottom-right" class="uk-card-default uk-list uk-width-auto header-dropdown uk-box-shadow-medium">
+                    <li className="uk-padding-small ">
+                        <span is uk-icon="icon: settings" class="uk-margin-small-right uk-margin-small-left"></span>
+                        <span onClick={this.logOutFromFacebookHandler.bind(this)}>Настройки</span>
+                    </li>
+                    <li className="uk-padding-small ">
+                        <span is uk-icon="icon: user" class="uk-margin-small-right uk-margin-small-left"></span>
+                        <span onClick={this.logOutFromFacebookHandler.bind(this)}>Аккаунт</span>
+                    </li>
+                    <li className="uk-padding-small">
+                        <span is uk-icon="icon: sign-out" class="uk-margin-small-right uk-margin-small-left"></span>
+                        <span onClick={this.logOutFromFacebookHandler.bind(this)}>Выйти</span>
+                    </li>
+                </ul>
             </ul>
         } else if (user.isLogged == 'no') {
             authTemplate = <button is class="uk-button uk-button-text uk-animation-fade" onClick={this.loginToFacebookHandler.bind(this)}>Facebook</button>
         }
         return (
-            <nav is class="uk-navbar-container uk-margin-bottom" uk-navbar uk-sticky="bottom: #offset">
-                <div className="uk-navbar-left">
-                    <a className="uk-navbar-item uk-logo" href="#">
-                        <img src="images/logo.svg" alt=""/>
-                    </a>
-                </div>
-                <div className="uk-navbar-right">
-                    <div className="uk-navbar-item">
-                        {authTemplate}
+            <div>
+                <nav is class="uk-navbar-container" uk-navbar uk-sticky="bottom: #offset">
+                    <div className="uk-navbar-left">
+                        <span class="uk-navbar-item uk-icon-link uk-hidden@s" is uk-toggle="target: #offcanvas-push">
+                            <span is uk-icon="icon: menu; ratio: 1.5"/>
+                        </span>
+                        <a className="uk-navbar-item uk-logo uk-visible@s" href="#">
+                            <img src="images/logo.svg" alt=""/>
+                        </a>
                     </div>
-                </div>
-                <div className="loading-alert uk-animation-fast uk-animation-slide-top-small"></div>
-            </nav>
+                    <div className="uk-navbar-center">
+                        <a className="uk-navbar-item uk-logo uk-hidden@s" href="#">
+                            <img src="images/logo.svg" alt=""/>
+                        </a>
+                    </div>
+                    <div className="uk-navbar-right">
+                        <div className="uk-navbar-item">
+                            {authTemplate}
+                        </div>
+                    </div>
+                    <div className="loading-alert uk-animation-fast uk-animation-slide-top-small"></div>
+                    <div is id="offcanvas-push" uk-offcanvas="mode: reveal; overlay: true" class="uk-animation uk-width-1-1">
+                        <div className="uk-offcanvas-bar">
+                            <h3>Крутая надпись</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
         )
     }
 }
